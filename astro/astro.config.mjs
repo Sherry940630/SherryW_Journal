25fe0637e9f7e5e34663d912a7f3272c4115987b
SherryW <sherry94063023@gmail.com>
2026-06-29 20:21:15 +0800

    Github Workflow File
    idk how this works

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://Sherry940630.github.io',
+    base: '/SherryW_Journal',
+});
